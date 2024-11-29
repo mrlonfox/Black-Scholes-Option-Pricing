5d9f1a6af287ff525ca3b13d01fc308a0eb0147c
Marlon Fox <148476952+mrlonfox@users.noreply.github.com>
2024-11-29 23:56:08 +0100

    calculate_volatility function added

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,5 +1,7 @@
-# Data fetching
+"""Data fetching"""""
+
 import yfinance as yf
+import numpy as np
 
 def get_stock_data(ticker):
     """
@@ -8,7 +10,19 @@ def get_stock_data(ticker):
     :return: Current stock price (Spot Price).
     """
     stock = yf.Ticker(ticker)
-    current_price = stock.history(period="1d")['Close'][0] 
+    current_price = stock.history(period="1d")['Close'][0]
     return current_price
 
-
+def calculate_volatility(ticker, lookback_period=252):
+    """
+    Calculates the historical volatility of a stock based on its past prices.
+    :param ticker: Stock ticker symbol.
+    :param lookback_period: Number of days to calculate historical 
+    volatility (default: 252 trading days).
+    :return: Annualized historical volatility (σ).
+    """
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=f"{lookback_period}d")['Close']
+    log_returns = np.log(data / data.shift(1)).dropna()
+    volatility = np.std(log_returns) * np.sqrt(252)
+    return volatility
