47beed7dc37c6abc05f5bd67bc1ad547e758d441
Marlon Fox <148476952+mrlonfox@users.noreply.github.com>
2024-11-29 23:50:35 +0100

    get_stock_data function added

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,1 +1,14 @@
 # Data fetching
+import yfinance as yf
+
+def get_stock_data(ticker):
+    """
+    Fetches the current stock price for the given ticker.
+    :param ticker: Stock ticker symbol.
+    :return: Current stock price (Spot Price).
+    """
+    stock = yf.Ticker(ticker)
+    current_price = stock.history(period="1d")['Close'][0] 
+    return current_price
+
+
